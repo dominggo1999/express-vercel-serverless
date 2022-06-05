9eaf873551751d45aa0e84b357cd2ea3862b8ac2
dominggo1999 <arnolddominggo19@gmail.com>
2022-06-05 16:33:03 +0700

    add README

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/api', (req, res) => {
-  res.send('<h1>Ttest</h1>');
+  res.send('<h1>Hello World</h1>');
 });
 
 module.exports = app;
